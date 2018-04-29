2b9402a4b3f41e25b26427f66d6ceab9c99f72c4
maxim-h <maksim.holmatov@gmail.com>
2018-04-29 12:06:05 +0300

    Preliminary results
    Somewhat complete. Pathway visualized, PCA done. No luck(

--- Expr.R
+++ Expr.R
@@ -1,22 +1,90 @@
 library(DESeq2)
 library(data.table)
 library(dplyr)
+library(KEGGprofile)
+library(biomaRt)
+library(plyr)
+library(ggplot2)
 
 
-directory <- "g12c"
-
-sampleFiles <- grep("counts",list.files(directory),value=TRUE)
-sampleCondition <- sub("(.*counts).*","\\1",sampleFiles)
-sampleTable <- data.frame(sampleName = sampleFiles,
-                          fileName = sampleFiles,
-                          condition = sampleCondition)
 
 
-c <- read.table(gzfile("g12C/06db23e3-2dc3-4da9-915e-fe3d4a091de5.htseq.counts.gz"))
-d <- read.table(gzfile("g12C/09572b33-5517-462d-9a46-ec3d9b11b59b.htseq.counts.gz"))
-e <- read.table(gzfile("g12C/12c4510a-fbad-4bff-97b5-846d4da395bd.htseq.counts.gz"))
+sample_table <- function(directory){
+  sampleName <- grep("counts",list.files(directory),value=TRUE)
+  sampleCondition <- rep(directory, times=length(sampleName))
+  sampleFiles = paste0(directory, "/", sampleName)
+  return(cbind(sampleName, sampleFiles, sampleCondition))
+}
 
 
-l <- lapply(paste0("g12c/",list.files("g12c")), function(x) read.table(gzfile(x)))
-r <- bind_cols(l)
-r <- r[, c(1, seq(2, ncol(r), 2))]
+sampleTable <- sub(".tsv", "", list.files(".", pattern = ".tsv")) %>% lapply(., sample_table) %>% do.call(rbind, .) %>% data.frame
+
+sampleTable <- sampleTable[!duplicated(sampleTable$sampleName),]
+
+
+
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
+                                       design= ~ sampleCondition)
+
+dds <- DESeq(ddsHTSeq)
+
+vst_dds <- vst(dds)
+
+#counts.norm <- assay(vst_dds)
+plotPCA(vst_dds, intgroup=c("sampleCondition")) + theme_bw()
+
+
+c <- counts(dds, normalized=T)
+rownames(c) <- gsub("\\..*$", "", rownames(c))
+
+conv <- convertId(c, filters="entrezgene")
+
+ens <- gsub("\\..*$", "", rownames(c))
+
+length(unique(ens)) == length(ens)
+
+
+
+mart<- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+ensembl_genes<- rownames(c)
+
+bm <- getBM(
+  filters= "ensembl_gene_id", 
+  attributes= c("ensembl_gene_id", "entrezgene", "description"),
+  values= ensembl_genes,
+  mart= mart)
+
+bm$description <- NULL
+
+c <- data.frame(c)
+
+c$ensgene <- rownames(c)
+
+m <- merge(c, bm, by.x = "ensgene", by.y = "ensembl_gene_id")
+
+m <- data.table(m)
+m$ensgene <- NULL
+
+a <- aggregate(. ~ entrezgene, data=m, median)
+
+rownames(a) <- a$entrezgene
+a$entrezgene <- NULL
+
+
+plot_pathway(a[,!grepl("13", sampleTable$sampleCondition)], pathway_id = "04012",species='hsa', type="lines", result_name="12.png")
+
+
+erbb <- c("1956", "2064", "2065", "2066")
+
+erbb_expr <- a[rownames(a) %in% erbb,]
+
+
+pl <- data.frame(list(erbb=colMeans(as.matrix(erbb_expr)), codon=sampleTable$sampleCondition, file=sampleTable$sampleName))
+
+ggplot(data=pl, aes(x=codon,y=erbb,  fill=codon))+
+  geom_violin()
+
+## Pre-Filtering. Remove rows with few counts
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
